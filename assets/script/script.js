93bd24bc20b0d6537369ddfa7659df1d38e850d6
merdonm <merdonm2001@gmail.com>
2024-07-29 00:17:05 +0530

    tempp

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        dots:true,
+        Infinite:true,
+        slidesToShow : 1,
+        slidesToScroll : 1,
+        arrows : true
+    })
+})
--- script/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.slick-carousel').slick({
-        dots:true,
-        Infinite:true,
-        slidesToShow : 1,
-        slidesToScroll : 1,
-        arrows : true
-    })
-})
